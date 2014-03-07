93a6875515996ba29cf8ea4f14d4913dcce699bb
Joel Low <joel@joelsplace.sg>
2014-03-07 23:46:35 +0800

    Record the proper protocol version, and also send the blank image all the time.

--- app/controllers/collector_controller.rb
+++ app/controllers/collector_controller.rb
@@ -4,19 +4,23 @@ class CollectorController < ApplicationController
     protocol = params[:protocol]
     case protocol
       when 'ssl3'
+        protocol = 0
         cookie_sym = :client_collected_ssl3
       when 'tls1'
+        protocol = 1
         cookie_sym = :client_collected_tls1
       when 'tls1.1'
+        protocol = 2
         cookie_sym = :client_collected_tls11
       when 'tls1.2'
+        protocol = 3
         cookie_sym = :client_collected_ssl12
       else
-        return
+        return send_image
     end
 
     if cookies[cookie_sym] then
-      return
+      return send_image
     end
     cookies[cookie_sym] = true
 
@@ -26,10 +30,16 @@ class CollectorController < ApplicationController
                           useragent: headers['HTTP_USER_AGENT']
                         })
 
+    send_image
+  end
+
+private
+  def send_image
     blank_image = File.open(File.join(Rails.root, 'app/assets/images/blank.png'), 'rb') do |f|
       f.read
     end
+    
     send_data(blank_image, type: 'image/png',
-                           disposition: 'inline')
+              disposition: 'inline')
   end
 end
--- app/models/client_sample.rb
+++ app/models/client_sample.rb
@@ -1,2 +1,3 @@
 class ClientSample < ActiveRecord::Base
+  PROTOCOLS = ['SSLv3', 'TLSv1', 'TLSv1.1', 'TLSv1.2']
 end

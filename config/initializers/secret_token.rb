26c2216f3d2107e46660ce4064813ecf387c0e7c
Joel Low <joel@joelsplace.sg>
2014-03-07 20:08:30 +0800

    Initialise my Rails app for collecting CS2107 SSL statistics.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClientSampler::Application.config.secret_key_base = '629edf4c150dce014803b279b9ed512b308cfd4481f5e8cc67da38e4aba6dd4b07837ae15454fb98376d7d95d0de03e0082cc34ceb83864fb19c5c580934d016'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ClientSampler::Application.config.session_store :cookie_store, key: '_Client Sampler_session'

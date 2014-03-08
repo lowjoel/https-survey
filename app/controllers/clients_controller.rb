24d583f2836f40669b13a36a13f41477d0e31752
Joel Low <joel@joelsplace.sg>
2014-03-08 14:24:41 +0800

    Display SSL request statistics first.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,9 +1,20 @@
 class ClientsController < ApplicationController
   def index
-
+    # Compute aggregate statistics
+    @ssl_versions = map_versions_to_strings(ClientSample.group(:protocol).count)
   end
 
   def privacy
-    
+
+  end
+
+private
+  def map_versions_to_strings(versions)
+    result = {}
+    versions.each_pair do |key, value|
+      result[ClientSample::PROTOCOLS[key]] = value
+    end
+
+    return result
   end
 end

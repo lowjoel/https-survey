ffe295aa0f6dccb5ec909b5d2d7d42d0870d97b3
Joel Low <joel@joelsplace.sg>
2014-03-08 11:11:10 +0800

    Add a blank hook for the before save -- we will parse the user-agent string later.

--- app/models/client_sample.rb
+++ app/models/client_sample.rb
@@ -1,3 +1,11 @@
 class ClientSample < ActiveRecord::Base
+  before_save :sync_client_info
+
   PROTOCOLS = ['SSLv3', 'TLSv1', 'TLSv1.1', 'TLSv1.2']
+
+private
+  # Synchronises the useragent string with the individual columns.
+  def sync_client_info
+
+  end
 end

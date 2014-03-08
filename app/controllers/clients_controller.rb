dfd4c231e9f4e28965f0667e2cb173be11c51d6c
Joel Low <joel@joelsplace.sg>
2014-03-08 15:01:59 +0800

    Also allow us to see the TLS implementations broken down by platform.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,6 +2,8 @@ class ClientsController < ApplicationController
   def index
     # Compute aggregate statistics
     @ssl_versions = map_versions_to_strings(ClientSample.group(:protocol).count)
+    @ssl_versions_by_platform = ClientSample.group(:platform_id, :protocol).count
+    @ssl_versions_by_platform = map_platforms_to_strings(@ssl_versions_by_platform)
   end
 
   def privacy
@@ -16,5 +18,29 @@ private
     end
 
     return result
+  end
+
+  def map_platforms_to_strings(platforms)
+    result = {}
+    all_platforms = {}
+    ClientPlatform.all.each do |p|
+      all_platforms[p.id] = p.platform
+    end
+
+    platforms.each_pair do |key, value|
+      platform = all_platforms[key[0]]
+      protocol = key[1]
+      result[platform] = {} unless result[platform]
+      result[platform][protocol] = value
+    end
+
+    result2 = {}
+    result.each_pair do |platform, value|
+      map_versions_to_strings(value).each_pair do |protocol, value|
+        result2["#{platform}, #{protocol}"] = value
+      end
+    end
+
+    return result2
   end
 end

225ad63acd734ac4373d36a456719a2c85e3c68e
Joel Low <joel@joelsplace.sg>
2014-03-12 16:00:18 +0800

    Define my Clockwork script. We have two events -- maintain Alexa ranks, and maintain site SSLLab test results.

---
+++ lib/clock.rb
@@ -0,0 +1,19 @@
+require 'clockwork'
+
+module Clockwork
+  handler do |job|
+    puts "Running #{job}"
+  end
+
+  # handler receives the time when job is prepared to run in the 2nd argument
+  # handler do |job, time|
+  #   puts "Running #{job}, at #{time}"
+  # end
+
+  every(1.day, 'alexa.refresh', :at => '00:00') do
+    Delayed::Job.enqueue AlexaTopSitesCrawler.new
+  end
+  every(1.day, 'ssl_test.refresh', :at => '00:30') do
+    Delayed::Job.enqueue MaintainTestResults.new
+  end
+end

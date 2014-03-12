ee2d7453785582587b8fa6c7ea59b57e38314414
Joel Low <joel@joelsplace.sg>
2014-03-12 15:50:59 +0800

    Document what it does.

--- app/models/server_ssl_test.rb
+++ app/models/server_ssl_test.rb
@@ -1,4 +1,5 @@
 class ServerSslTest < ActiveRecord::Base
+  # Selects the latest test for each server.
   scope :latest, -> { joins('INNER JOIN
     (SELECT server_most_visit_id, last_tested FROM server_ssl_tests GROUP BY last_tested) sst ON
     server_ssl_tests.server_most_visit_id=sst.server_most_visit_id AND

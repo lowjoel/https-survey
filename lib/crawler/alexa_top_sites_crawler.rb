e47e6f77b80452d47f7fa591c79306593311c25b
Joel Low <joel@joelsplace.sg>
2014-03-12 15:20:15 +0800

    Factor out the code, we will crawl in one function, and we will rank in another.

--- lib/crawler/alexa_top_sites_crawler.rb
+++ lib/crawler/alexa_top_sites_crawler.rb
@@ -4,6 +4,16 @@ require 'open-uri'
 
 class AlexaTopSitesCrawler
   def perform
+    urls = get_urls
+    update_top_500(urls)
+  end
+
+private
+  # The URL for the n'th page of the top sites page
+  ALEXA_SG_TOP_SITES_PAGE = 'http://www.alexa.com/topsites/countries;%d/SG'
+
+  # Gets the URLs that are in the Alexa Top 500. They are ordered according to their position.
+  def get_urls
     sites = []
     for page in 0..19 do
       url = ALEXA_SG_TOP_SITES_PAGE % [page]
@@ -17,11 +27,22 @@ class AlexaTopSitesCrawler
 
       sleep(1)
     end
+
+    return sites
   end
 
-private
-  # The URL for the n'th page of the top sites page
-  ALEXA_SG_TOP_SITES_PAGE = 'http://www.alexa.com/topsites/countries;%d/SG'
+  # Updates the list of top 500 sites. New sites are added, old sites are set to rank null.
+  def update_top_500(sites)
+    ServerMostVisit.transaction do
+      ServerMostVisit.update_all 'rank = NULL'
+
+      sites.each_index do |i|
+        site = ServerMostVisit.find_or_create_by_site(sites[i])
+        site.rank = i + i
+        site.save
+      end
+    end
+  end
 end
 
 (AlexaTopSitesCrawler.new).perform

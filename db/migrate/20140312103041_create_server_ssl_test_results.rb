0425a884a32b384314e6f7173c4018ac5372fcd2
Joel Low <joel@joelsplace.sg>
2014-03-12 18:33:11 +0800

    Reference the parent table.

--- db/migrate/20140312103041_create_server_ssl_test_results.rb
+++ db/migrate/20140312103041_create_server_ssl_test_results.rb
@@ -1,13 +1,14 @@
 class CreateServerSslTestResults < ActiveRecord::Migration
   def change
     create_table :server_ssl_test_results do |t|
-      t.string ip
+      t.integer :server_ssl_test_id
+      t.string :ip
       t.boolean :ssl3
       t.boolean :tls1
       t.boolean :tls11
       t.boolean :tls12
       t.integer :grade
-      
+
       t.timestamps
     end
   end

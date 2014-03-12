55b684ebd982645e9cdf08445a226a8a74e16862
Joel Low <joel@joelsplace.sg>
2014-03-12 15:50:41 +0800

    Implemented the latest scope.

--- app/models/server_ssl_test.rb
+++ app/models/server_ssl_test.rb
@@ -1,2 +1,6 @@
 class ServerSslTest < ActiveRecord::Base
+  scope :latest, -> { joins('INNER JOIN
+    (SELECT server_most_visit_id, last_tested FROM server_ssl_tests GROUP BY last_tested) sst ON
+    server_ssl_tests.server_most_visit_id=sst.server_most_visit_id AND
+    server_ssl_tests.last_tested=sst.last_tested') }
 end

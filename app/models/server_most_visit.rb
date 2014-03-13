7a2c2fd8cb52b2290cd0a89c62249a4cbd49e414
Joel Low <joel@joelsplace.sg>
2014-03-13 08:55:31 +0800

    Fix the models' scopes.

--- app/models/server_most_visit.rb
+++ app/models/server_most_visit.rb
@@ -1,7 +1,8 @@
 class ServerMostVisit < ActiveRecord::Base
-  scope :tests, -> {
-    join('LEFT OUTER JOIN server_ssl_tests ON server_most_visits.id=server_ssl_tests.server_most_visit_id')
+  scope :with_untested, -> {
+    joins('LEFT OUTER JOIN server_ssl_tests ON server_most_visits.id=server_ssl_tests.server_most_visit_id').
+    where('server_ssl_tests.id IS NULL')
   }
-  
-  has_many :server_ssl_test, alias: :tests
+
+  has_many :tests, class_name: :server_ssl_test
 end
--- app/models/server_ssl_test.rb
+++ app/models/server_ssl_test.rb
@@ -1,9 +1,9 @@
 class ServerSslTest < ActiveRecord::Base
   # Selects the latest test for each server.
-  scope :latest, -> { joins('INNER JOIN
+  scope :with_latest, -> { joins('INNER JOIN
     (SELECT server_most_visit_id, last_tested FROM server_ssl_tests GROUP BY last_tested) sst ON
     server_ssl_tests.server_most_visit_id=sst.server_most_visit_id AND
     server_ssl_tests.last_tested=sst.last_tested') }
 
-  belongs_to :server_most_visit
+  belongs_to :server, class_name: :ServerMostVisit
 end

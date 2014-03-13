a300130a2153eb4023cf06b0208c8012b4434207
Joel Low <joel@joelsplace.sg>
2014-03-13 08:32:19 +0800

    Allow us to find all tests, and also find the tests associated with a server-most-visited.

--- app/models/server_most_visit.rb
+++ app/models/server_most_visit.rb
@@ -1,2 +1,7 @@
 class ServerMostVisit < ActiveRecord::Base
+  scope :tests, -> {
+    join('LEFT OUTER JOIN server_ssl_tests ON server_most_visits.id=server_ssl_tests.server_most_visit_id')
+  }
+  
+  has_many :server_ssl_test, alias: :tests
 end

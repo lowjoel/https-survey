2360a04ec6d63db5d2baf5957546bbb00c0a9458
Joel Low <joel@joelsplace.sg>
2014-03-14 10:03:49 +0800

    Only cause tests to be re-run after 7 days; tests between 1 and 7 days will be randomly re-run to distribute the update queue length over time.

--- lib/crawler/maintain_test_results.rb
+++ lib/crawler/maintain_test_results.rb
@@ -11,9 +11,20 @@ module Crawler
       end
 
       # Refresh all out-of-date tests
-      ServerSslTest.with_latest.where('last_tested < ?', Time.now - 1.day) do |test|
+      ServerSslTest.with_latest.where('last_tested < ?', Time.now - 7.day) do |test|
+        Delayed::Job.enqueue SsllabsCrawler.new(test.server.url)
+      end
+
+      # Refresh a smattering of those which are up-to-date so we stagger the update times
+      tests_count = ServerSslTest.with_latest.count
+      ServerSslTest.with_latest.where('server_ssl_tests.last_tested > ? AND
+                                       server_ssl_tests.last_tested < ?',
+                                      Time.now - 7.day, Time.now - 1.day).
+                                random(tests_count / 10) do |test|
         Delayed::Job.enqueue SsllabsCrawler.new(test.server.url)
       end
     end
   end
 end
+
+Crawler::MaintainTestResults.new.perform

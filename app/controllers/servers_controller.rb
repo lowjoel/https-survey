bcd0aaa56a88a6185cb3bbdbb89d2893f6e36694
Joel Low <joel@joelsplace.sg>
2014-03-19 18:46:09 +0800

    Rewrite the query to be more efficient (200ms -> 2ms)

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -8,15 +8,13 @@ class ServersController < ApplicationController
     }
     ratings = [0, 0, 0, 0, 0, 0, 0, 0]
 
-    ServerSslTest.with_latest.each do |site|
-      site.results.each do |result|
-        @protocols['SSL v3'] += result.ssl3 ? 1 : 0
-        @protocols['TLS v1'] += result.tls1 ? 1 : 0
-        @protocols['TLS v1.1'] += result.tls11 ? 1 : 0
-        @protocols['TLS v1.2'] += result.tls12 ? 1 : 0
-        if result.grade then
-          ratings[result.grade] += 1
-        end
+    ServerSslTestResult.all.joins(:server_ssl_test).merge(ServerSslTest.with_latest).each do |result|
+      @protocols['SSL v3'] += result.ssl3 ? 1 : 0
+      @protocols['TLS v1'] += result.tls1 ? 1 : 0
+      @protocols['TLS v1.1'] += result.tls11 ? 1 : 0
+      @protocols['TLS v1.2'] += result.tls12 ? 1 : 0
+      if result.grade then
+        ratings[result.grade] += 1
       end
     end
 

9e58b24b30a54ecd871e39a6228000656a2b289a
Joel Low <joel@joelsplace.sg>
2014-05-03 18:25:30 +0800

    Wrong group-by clause...

--- app/models/server_ssl_test.rb
+++ app/models/server_ssl_test.rb
@@ -1,7 +1,7 @@
 class ServerSslTest < ActiveRecord::Base
   # Selects the latest test for each server.
   scope :with_latest, -> { joins('INNER JOIN
-    (SELECT server_most_visit_id, last_tested FROM server_ssl_tests GROUP BY last_tested) sst ON
+    (SELECT server_most_visit_id, last_tested FROM server_ssl_tests GROUP BY server_most_visit_id) sst ON
     server_ssl_tests.server_most_visit_id=sst.server_most_visit_id AND
     server_ssl_tests.last_tested=sst.last_tested') }
 
